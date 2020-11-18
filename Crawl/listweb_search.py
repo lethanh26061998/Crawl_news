97389d799cfd27eafb4e94e91df0a15cb747788c
Le Thi Thanh <43176550+lethanh26061998@users.noreply.github.com>
2020-11-18 12:26:25 +0700

    Update listweb_search.py

--- Crawl/listweb_search.py
+++ Crawl/listweb_search.py
@@ -1,18 +1,18 @@
 dict_data = {
 # 1 item bao gom cac truong du lieu: title, content, time, tag, outlink, search_input, next_page, isloadmore,links
 #1-next: ok -> ok
-# 'https://vietnammoi.vn':
-#     [
-#     '//h1[@id="title-article"]',
-#     ['//div[@class="sapo margin-20-bottom"]', '//div[@class="entry-body"]//p'],
-#     '//span[@class="time left"]/span',
-#     '',
-#     ['//ul[@class="related-news margin-25-bottom detailpopup"]//li//a'],
-#     '//*[@id="search"]',
-#     '(//ul[@class="pager right margin-25-top"]/li)[last()]',
-#     '',
-#     '//ul[@class="news-stream clearafter"]/li/a'
-# ],
+'https://vietnammoi.vn':
+    [
+    '//h1[@id="title-article"]',
+    ['//div[@class="sapo margin-20-bottom"]', '//div[@class="entry-body"]//p'],
+    '//span[@class="time left"]/span',
+    '',
+    ['//ul[@class="related-news margin-25-bottom detailpopup"]//li//a'],
+    '//*[@id="search"]',
+    '(//ul[@class="pager right margin-25-top"]/li)[last()]',
+    '',
+    '//ul[@class="news-stream clearafter"]/li/a'
+],
 #2-xem them: ok
 'https://tuoitre.vn':
     ['//h1[@class="article-title"]',
@@ -26,18 +26,18 @@ dict_data = {
     '//ul[@class="list-news-content"]/li/a'
 ],
 #3-next: ok
-# 'https://vov.vn':
-# [
-    # '//h2[@class="article__title"]',
-    # ['//div[@class="article__head"]//h4//div', '//div[@class="text-long"]/p'],
-    # '//div[@class="article__time"]//a',
-    # '//div[@class="tags"]//a',
-    # ['//div[@class="news-list__content"]/div/div/a'],
-    # '//*[@id="edit-keyword"]',
-    # '//li[@class="pager__item pager__item--next"]//a',
-    # '',
-    # '//div[@class="views-element-container"]//div/div/div/a'
-# ],
+'https://vov.vn':
+[
+    '//h2[@class="article__title"]',
+    ['//div[@class="article__head"]//h4//div', '//div[@class="text-long"]/p'],
+    '//div[@class="article__time"]//a',
+    '//div[@class="tags"]//a',
+    ['//div[@class="news-list__content"]/div/div/a'],
+    '//*[@id="edit-keyword"]',
+    '//li[@class="pager__item pager__item--next"]//a',
+    '',
+    '//div[@class="views-element-container"]//div/div/div/a'
+],
 #4-xem them: 
 # 'https://kenh14.vn':
 # [
@@ -52,18 +52,18 @@ dict_data = {
     # '//ul[@class="knsw-list"]/li//div/a',
 # ],
 #5-next: ok
-# 'https://timkiem.vnexpress.net/':
-# [
-#     '//h1[@class="title-detail"]',
-#     ['//p[@class="description"]', '//article[@class="fck_detail "]//p'],
-#     '//span[@class="date"]',
-#     '//div[@class="tags"]//h2//a',
-#     ['//ul[@class="list-news  gaBoxLinkDisplay"]/li/a'],
-#     '//*[@id="search_q"]',
-#     '(//div[@class="button-page flexbox"]/a)[last()]',
-#     '',
-#     '//div[@class="width_common list-news-subfolder"]/article//div//a'
-# ],
+'https://timkiem.vnexpress.net/':
+[
+    '//h1[@class="title-detail"]',
+    ['//p[@class="description"]', '//article[@class="fck_detail "]//p'],
+    '//span[@class="date"]',
+    '//div[@class="tags"]//h2//a',
+    ['//ul[@class="list-news  gaBoxLinkDisplay"]/li/a'],
+    '//*[@id="search_q"]',
+    '(//div[@class="button-page flexbox"]/a)[last()]',
+    '',
+    '//div[@class="width_common list-news-subfolder"]/article//div//a'
+],
 # 'https://ncov.moh.gov.vn':
 # [
     # '//h3[@class="header-title"]//span',
